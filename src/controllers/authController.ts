a347e59fa9ccc6eeafb84e41d23eb2420ff6db70
alinguyen197 <alinguyen197@gmail.com>
2025-11-17 22:12:30 +0700

    fix config source

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,18 +1,18 @@
-import { NextFunction, Request, Response } from 'express';
-import { IUser } from '../interfaces';
-import authServices from '../services/authService';
-import { ApiResponder } from '../utils/response.common';
+import { NextFunction, Request, Response } from 'express'
+import { IUser } from '../interfaces'
+import authServices from '../services/authService'
+import { ApiResponder } from '../utils/response.common'
 const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
     // Lấy dữ liệu từ request body
-    const data: IUser = req.body;
-    const user = await authServices.handleLogin(data);
-    return ApiResponder.success(res, user);
+    const data: IUser = req.body
+    const user = await authServices.handleLogin(data)
+    return ApiResponder.success(res, user)
   } catch (error: any) {
-    next(error);
+    next(error)
   }
-};
+}
 
 export default {
   login,
-};
+}
